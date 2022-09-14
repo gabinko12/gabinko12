5818bddd9e65e2dd6f33192d55c011d40866f339
Gabriel Pahljina Hernandez <gabriel.pahljinah12@gmail.com>
2022-09-14 17:59:41 +0200

    file updated

--- buttonClicked.js
+++ buttonClicked.js
@@ -1,13 +1,13 @@
 const songsEl = document.querySelector(".songs")
 const buttonShowEl = document.querySelector(".buttonShow")
 const buttonRemoveEl = document.querySelector(".buttonRemove")
-const bozicUskrsTextEl = document.querySelector(".bu_p")
+const bozicTextEl = document.querySelector("#bozic p")
 const videoEl = document.querySelector(".video")
 
 function buttonClicked(){
     songsEl.classList.toggle("active")
     buttonShowEl.classList.toggle("remove")
     videoEl.classList.toggle("remove")
-    bozicUskrsTextEl.classList.toggle("remove")
+    bozicTextEl.classList.toggle("remove")
     buttonRemoveEl.classList.toggle("active2")
 }
