f246cf61ae66ec6d56ae96e9a95fc905b982ab04
Gabriel Pahljina Hernandez <gabriel.pahljinah12@gmail.com>
2022-09-19 20:16:05 +0200

    file updated

--- buttonClicked.js
+++ buttonClicked.js
@@ -6,11 +6,13 @@ const buttonShowEl = document.querySelector(".buttonShow")
 const buttonRemoveEl = document.querySelector(".buttonRemove")
 const buttonShowUskrsEl = document.querySelector(".buttonShowUskrs")
 const buttonRemoveUskrsEl = document.querySelector(".buttonRemoveUskrs")
-/* Text && video */
+const buttonFligImages = document.querySelector(".flig-images")
+/* Text, video and images */
 const bozicTextEl = document.querySelector("#bozic p")
 const uskrsTextEl = document.querySelector("#uskrs p")
 const videoEl = document.querySelector(".video")
 const video1El = document.querySelector(".video1")
+const fligImages = document.querySelector("flig-images-wrapper")
 
 /* Functions */
 function buttonClicked(){
@@ -26,4 +28,7 @@ function buttonClickedUskrs(){
     buttonRemoveUskrsEl.classList.toggle("active2")
     uskrsTextEl.classList.toggle("remove")
     video1El.classList.toggle("remove")
+}
+function buttonClickedFlig(){
+    fligImages.classList.toggle("imagesActive")
 }
